e3c86439d190ff2fc6bdd8a1f5c8f3b6325d9c21
Milton Timana <miltonkevin10@gmail.com>
2025-10-16 08:23:17 +0200

    first commit

---
+++ mutex_semaforo.py
@@ -0,0 +1,5 @@
+import threading
+import time
+import random
+import argparse
+
---
+++ test_env.py
@@ -0,0 +1,4 @@
+import sys
+print('Python is ready!')
+print('Executable:', sys.executable)
+print('Version:', sys.version)
